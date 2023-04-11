bc77a1bd59fb5944cd81c183b41ed2e1835844cf
Phodal Huang <h@phodal.com>
2023-04-11 16:48:47 +0800

    docs: try to add prompts types

--- code/text-to-repository.py
+++ code/text-to-repository.py
@@ -1,13 +1,28 @@
 import json
 import csv
 
-with open('../datasets/sql/repository-5k.jsonl', 'r') as f:
+type_map = {}
+# open datasets/sql/types.json
+
+
+# open datasets/sql/prompts.json
+# create id map for prompts in datasets/sql/prompts.json
+id_prompt_map = {}
+with open('../datasets/sql/prompts.json', 'r') as f:
+    data = json.loads(f.read())
+    for row in data:
+        id_prompt_map[row['id']] = row['prompt']
+
+
+with open('../datasets/sql/repositories-5k.jsonl', 'r') as f:
     data = [json.loads(row) for row in f.readlines()]
 
     with open('../datasets/sql/repository-5k-train.jsonl', 'w') as f:
         for row in data:
+            print(row)
+            id = int(row['id'])
             item = {
-                'instruction': 'text to kotlin repository',
+                'instruction': 'text to kotlin repository with class',
                 'input': row['output'],
                 'output': row['input']
             }
