0f9dbc468559ce03b16640633639a5384dfdd677
Phodal Huang <h@phodal.com>
2023-04-11 17:26:50 +0800

    fix: fix datas

--- code/text-to-repository.py
+++ code/text-to-repository.py
@@ -8,10 +8,10 @@ type_map = {}
 # open datasets/sql/prompts.json
 # create id map for prompts in datasets/sql/prompts.json
 id_prompt_map = {}
-with open('../datasets/sql/prompts.json', 'r') as f:
+with open('../datasets/sql/llm-prompts.json', 'r') as f:
     data = json.loads(f.read())
     for row in data:
-        id_prompt_map[row['id']] = row['prompt']
+        id_prompt_map[row['id']] = row['requiredType']
 
 
 with open('../datasets/sql/repositories-5k.jsonl', 'r') as f:
@@ -19,16 +19,20 @@ with open('../datasets/sql/repositories-5k.jsonl', 'r') as f:
 
     with open('../datasets/sql/repository-5k-train.jsonl', 'w') as f:
         for row in data:
-            print(row)
+            requiredType = ""
             id = int(row['id'])
+            if id in id_prompt_map:
+                # id_prompt_map[id] is a list, check if it is empty
+                if id_prompt_map[id]:
+                    requiredType = "###" + " ".join(id_prompt_map[id]) + "###"
+
             item = {
                 'instruction': 'text to kotlin repository with class',
-                'input': row['output'],
+                'input': row['output'] + "\n" + requiredType,
                 'output': row['input']
             }
 
-        f.write(json.dumps(item) + '\n')
-
+            f.write(json.dumps(item) + '\n')
 
     # 5kl to csv
     with open('../datasets/sql/repository-5k.csv', 'w') as f:
