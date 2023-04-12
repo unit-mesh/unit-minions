433cf986c70987d850bb18ad6996e46531e5a391
Phodal Huang <h@phodal.com>
2023-04-12 14:06:15 +0800

    docs: fix for format

--- demos/domain/pdf-to-text.py
+++ demos/domain/pdf-to-text.py
@@ -1,9 +1,25 @@
+import json
 import re
 
 import PyPDF2
 
 title_pattern = r'^[一二三四五六七八九十零〇百千万亿]+、(.*?)$'
 subtitle_pattern = r'^（[一二三四五六七八九十零〇百千万亿]+）(.*?)$'
+
+
+def is_valid_subtitle(title: str) -> bool:
+    # Check if the title matches the pattern
+    if re.match(subtitle_pattern, title):
+        return True
+    else:
+        return False
+
+
+def get_valid_subtitle(title: str) -> str:
+    if re.match(subtitle_pattern, title):
+        return title.strip().split('）')[1].strip()
+    else:
+        return ""
 
 
 # a valid title should start with a chinese_numbers then a 、, like `十一、基金的财产`
@@ -41,7 +57,7 @@ def read_pdf_file(file: str) -> list[str]:
             page_text = re.sub(r'招商基金管理有限公司\s+招募说明书\s+\d+', '', page_text)
 
             # remove continues empty lines which are in the form of `\n\n\n\n`
-            page_text = re.sub(r'\n\s*\n', '', page_text)
+            page_text = re.sub(r'\n\n', '', page_text)
 
             text_list.append(page_text)
 
@@ -56,13 +72,42 @@ if __name__ == '__main__':
     # merge all the text into one string
     page_text = ''.join(text_list)
 
-    lines = page_text.split('\n')
-    for line in lines:
-        if is_valid_title(line):
-            # print(line)
-            question_answers.append({
-                'question': get_valid_title(line),
-                'answer': ''
-            })
+    # write page_text to a file
+    with open("domain-pdf.txt", "w") as fp:
+        fp.write(page_text)
 
-    print(question_answers)
+    lines = page_text.split('\n')
+
+    start_question = False
+    answer = ""
+    last_question = ""
+    for line in lines:
+        if is_valid_title(line.strip()):
+            start_question = False
+            continue
+
+        if is_valid_subtitle(line.strip()):
+            print(line)
+            if len(answer) > 0 and len(last_question) > 0:
+                question_answers.append((last_question, answer))
+                answer = ""
+
+            last_question = get_valid_title(line)
+            start_question = True
+
+        if start_question:
+            answer += line
+
+    question_answers.append({
+        "question": last_question,
+        "answer": answer
+    })
+
+    print("question size: ", len(question_answers))
+    # write question_answers to jsonl file
+    with open("domain-pdf.jsonl", "w") as fp:
+        for qa in question_answers:
+            fp.write(json.dumps(qa))
+            fp.write('\n')
+
+
